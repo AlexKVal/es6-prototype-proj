b2f8731a2a29ed3ac6d7810754cb7711e968ceca
AlexKVal <alexkval@gmail.com>
2015-08-10 01:04:41 +0300

    [changed] check wrong github token

--- src/index.js
+++ src/index.js
@@ -2,4 +2,3 @@ console.log('source file');
 console.log('has to change for bower');
 console.log('another one change for bower');
 console.log('another one change for bower');
-console.log('another one change for bower');
