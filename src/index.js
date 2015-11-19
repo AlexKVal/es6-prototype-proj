24ee38ee51476c69303707cba1f9971c1b8e1cc1
AlexKVal <alexkval@gmail.com>
2015-11-19 09:20:38 +0200

    [changed] sedond

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 console.log('source file');
 console.log('source file');
 console.log('has to change for bower');
-console.log('this is pre alpha');
+console.log('this is pre second');
 console.log('this is pre alpha');
 console.log('this is pre beta');
